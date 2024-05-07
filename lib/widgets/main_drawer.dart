39d5891982b8490e687057990e17a831e5a463dc
IdealyKanteCo <idealy@kanteco.com>
2024-05-07 15:58:05 +0300

    Update main_drawer.dart

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -12,7 +12,6 @@ class _MainDrawerState extends State<MainDrawer> {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        // Important: Remove any padding from the ListView.
         padding: EdgeInsets.zero,
         children: [
           const DrawerHeader(
